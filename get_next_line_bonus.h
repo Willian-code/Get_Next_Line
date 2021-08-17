52dad315271d976ea176694c704775ed7dc2592f
Willian-code <willianrosendodelima@outlook.com>
2021-08-17 17:00:42 -0300

    again

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/17 16:31:11 by wrosendo          #+#    #+#             */
+/*   Updated: 2021/08/17 16:40:42 by wrosendo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/17 16:31:21 by wrosendo          #+#    #+#             */
+/*   Updated: 2021/08/17 16:36:46 by wrosendo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t	len;
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	len = i;
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	str = malloc(i + 1);
+	if (str == 0)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = *str;
+		str++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*p;
+	size_t	src_len;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0')
+		i++;
+	src_len = i;
+	i = 0;
+	if (src_len > start)
+		while (s[i + start] && i < len)
+			i++;
+	else
+		return (ft_strdup(""));
+	p = (char *)malloc(sizeof(char) * (i + 1));
+	if (!p)
+		return (NULL);
+	ft_strlcpy(p, s + start, i + 1);
+	return (p);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	len1 = i;
+	i = 0;
+	while (s2[i] != '\0')
+		i++;
+	len2 = i;
+	str = NULL;
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s1, len1 + 1);
+	ft_strlcpy(str + len1, s2, (len2 + 1));
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s + i);
+	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	count;
+
+	count = 0;
+	if (!dst || !src)
+		return (0);
+	i = 0;
+	if (dstsize != 0)
+	{
+		while (src[i] != '\0' && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[count] != '\0')
+		count++;
+	return (count);
+}
